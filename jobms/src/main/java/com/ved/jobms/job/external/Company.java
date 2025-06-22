12c48dcf79dfccb3a00490af40e08e749420d68c
Vedant Upadhyay <vedantmahakal@gmail.com>
2025-06-22 16:30:18 +0530

    initial commit upto 11 adding OpenFeign

--- jobms/src/main/java/com/ved/jobms/job/external/Company.java
+++ jobms/src/main/java/com/ved/jobms/job/external/Company.java
@@ -9,7 +9,6 @@ public class Company {
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
--- jobms/src/main/java/com/ved/jobms/job/impl/JobServiceImpl.java
+++ jobms/src/main/java/com/ved/jobms/job/impl/JobServiceImpl.java
@@ -3,6 +3,8 @@ package com.ved.jobms.job.impl;
 import com.ved.jobms.job.Job;
 import com.ved.jobms.job.JobRepository;
 import com.ved.jobms.job.JobService;
+import com.ved.jobms.job.clients.CompanyClient;
+import com.ved.jobms.job.clients.ReviewClient;
 import com.ved.jobms.job.dto.JobDTO;
 import com.ved.jobms.job.external.Company;
 import com.ved.jobms.job.external.Review;
@@ -29,8 +31,14 @@ public class JobServiceImpl implements JobService {
     @Autowired
     RestTemplate restTemplate;
 
-    public JobServiceImpl(JobRepository jobRepository) {
+    private CompanyClient companyClient;
+
+    private ReviewClient reviewClient;
+
+    public JobServiceImpl(JobRepository jobRepository,CompanyClient companyClient,ReviewClient reviewClient) {
         this.jobRepository = jobRepository;
+        this.companyClient=companyClient;
+        this.reviewClient=reviewClient;
     }
 
     @Override
@@ -43,14 +51,17 @@ public class JobServiceImpl implements JobService {
 
     private JobDTO convertToDto(Job job){
 
-        Company company=restTemplate.getForObject("http://COMPANY-SERVICE:8081/companies/"+job.getCompanyId(), Company.class);
-        ResponseEntity<List<Review>> reviewResponse=restTemplate.exchange(
-                "http://REVIEW-SERVICE:8083/reviews?companyId=" + job.getCompanyId(),
-                HttpMethod.GET,
-                null,
-                new ParameterizedTypeReference<List<Review>>() {
-                });
-        List<Review> reviews=reviewResponse.getBody();
+//        Company company=restTemplate.getForObject("http://COMPANY-SERVICE:8081/companies/"+job.getCompanyId(), Company.class);
+      Company company= companyClient.getCompany(job.getCompanyId());
+
+//        ResponseEntity<List<Review>> reviewResponse=restTemplate.exchange(
+//                "http://REVIEW-SERVICE:8083/reviews?companyId=" + job.getCompanyId(),
+//                HttpMethod.GET,
+//                null,
+//                new ParameterizedTypeReference<List<Review>>() {
+//                });
+//        List<Review> reviews=reviewResponse.getBody();
+        List<Review> reviews=reviewClient.getReviews(job.getCompanyId());
         JobDTO jobDTO = JobMapper.mapToJobWithCompanyDto(job,company,reviews);
         return jobDTO;
     }
