3ae2d3f44ed97c22706cfbcc9f27b808e67c2797
Vedant Upadhyay <vedantmahakal@gmail.com>
2025-06-25 14:14:47 +0530

    initial commit upto 14 setting up API Gateway using spring cloud gateway

---
+++ gateway/src/test/java/com/ved/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ved.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
