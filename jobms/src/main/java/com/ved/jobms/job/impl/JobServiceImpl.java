31016f57cb42dfd880143f5a867bb3cab9ce7b5b
Vedant Upadhyay <vedantmahakal@gmail.com>
2025-06-25 18:25:34 +0530

    initial commit upto 15 Fault Tolerance with Circuit Breaker,Retry,Rate Limiter

--- jobms/src/main/java/com/ved/jobms/job/impl/JobServiceImpl.java
+++ jobms/src/main/java/com/ved/jobms/job/impl/JobServiceImpl.java
@@ -9,6 +9,9 @@ import com.ved.jobms.job.dto.JobDTO;
 import com.ved.jobms.job.external.Company;
 import com.ved.jobms.job.external.Review;
 import com.ved.jobms.job.mapper.JobMapper;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
@@ -42,11 +45,20 @@ public class JobServiceImpl implements JobService {
     }
 
     @Override
+//    @CircuitBreaker(name="companyBreaker",fallbackMethod = "companyBreakerFallback")
+//    @Retry(name="companyBreaker",fallbackMethod = "companyBreakerFallback")
+    @RateLimiter(name="companyBreaker",fallbackMethod = "companyBreakerFallback")
     public List<JobDTO> findAll() {
         List<Job> jobs=jobRepository.findAll();
         List<JobDTO> jobDTOS =new ArrayList<>();
 
         return jobs.stream().map(this::convertToDto).collect(Collectors.toList());
+    }
+
+    public List<String> companyBreakerFallback(Exception e){
+        List<String> list = new ArrayList<>();
+        list.add("Dummy");
+        return list;
     }
 
     private JobDTO convertToDto(Job job){
